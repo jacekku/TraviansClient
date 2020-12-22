78ce23ba19c156aeee00f326edc4f767cdd38984
jacekku <jacekwyroba97@gmail.com>
2020-12-22 21:06:36 +0100

    init

---
+++ client.js
@@ -0,0 +1,19 @@
+let websocket
+let name,data
+
+function connect() {
+    name = document.getElementById('name')
+
+    websocket = new WebSocket('ws://localhost:8080');
+    websocket.onopen = function (event) {
+        websocket.send(name.value + " connected")
+    };
+    websocket.onmessage = function (event) {
+        console.log(event.data);
+    }
+}
+
+function send() {
+    data = document.getElementById('data')
+    websocket.send(data.value)
+}
