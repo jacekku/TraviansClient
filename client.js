516d3940b0c258192d97d1dcfee4215b045730e4
jacekku <kecajwy@gmail.com>
2020-12-25 20:13:47 +0100

    basic client logic

--- client.js
+++ client.js
@@ -1,19 +1,82 @@
 let websocket
-let name, data
-
+let name
+let connected = false
 function connect() {
     name = document.getElementById('name')
 
-    websocket = new WebSocket('ws://traviansserver.herokuapp.com/:8080');
+    websocket = new WebSocket('ws://traviansserver.herokuapp.com');
+    // websocket = new WebSocket('ws://localhost:8080');
     websocket.onopen = function (event) {
-        websocket.send(name.value + " connected")
+        websocket.send(JSON.stringify({ type: 'connect', name: name.value, position: { x: 0, y: 0 } }))
+
     };
-    websocket.onmessage = function (event) {
-        console.log(event.data);
-    }
+    websocket.onmessage = handleGetMessage
+
+    websocket.onclose = handleDisconnected
 }
 
-function send() {
-    data = document.getElementById('data')
-    websocket.send(data.value)
+function move(newPosition) {
+    websocket.send(JSON.stringify({
+        type: 'move',
+        name: name.value,
+        position: newPosition
+    }))
+}
+
+function setEventListener() {
+    const blocks = document.querySelectorAll('.block')
+    blocks.forEach(block => {
+        block.addEventListener('click', (event) => {
+            const y = block.parentElement.id[1]
+            const x = block.id[1]
+            move({ x, y })
+        })
+    })
+}
+
+function handleConnected() {
+    const login = document.getElementById('login')
+    const game = document.getElementById('game')
+
+    game.classList.remove('hidden')
+    game.classList.add('visible')
+    login.classList.remove('visible')
+    login.classList.add('hidden')
+}
+function handleDisconnected() {
+    const login = document.getElementById('login')
+    const game = document.getElementById('game')
+
+    game.classList.add('hidden')
+    game.classList.remove('visible')
+    login.classList.add('visible')
+    login.classList.remove('hidden')
+}
+
+
+function mapPositionsToGrid(players) {
+    const blocks = document.querySelectorAll('.block')
+    blocks.forEach(block => block.innerHTML = '')
+    players.forEach(data => {
+        const name = data.name
+        const position = data.position
+        row = document.querySelector('#r' + position.y)
+        block = row.querySelector('#b' + position.x)
+        block.innerHTML += name + "<br/>"
+
+    })
+}
+
+function handleGetMessage(event) {
+    if (event.data == "ERROR") {
+        connected = false
+        return
+    }
+    if (event.data == "CONNECTED") {
+        connected = true
+        handleConnected()
+        setEventListener()
+        return
+    }
+    mapPositionsToGrid(JSON.parse(event.data))
 }
