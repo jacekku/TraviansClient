21dd12057d1dafd0c2c0c5326cce66fbfb140a54
jacekku <jacekwyroba97@gmail.com>
2021-08-22 22:01:49 +0200

    Day 5: migration to NestJS; migrating websockets part 1

--- client.js
+++ client.js
@@ -1,123 +1,132 @@
-let websocket
-let name
-let connected = false
+let socket;
+let name;
+let connected = false;
 let X = 1,
-    Y = 1
-let playerName
-terrain = ""
-players = ""
+  Y = 1;
+let playerName;
+terrain = "";
+players = "";
 
 function connect() {
-    name = document.getElementById('name')
-    playerName = name.value
-    // websocket = new WebSocket('wss://traviansserver.herokuapp.com/ws');
-    websocket = new WebSocket('ws://localhost:8080/ws');
-    websocket.onopen = function (event) {
-        websocket.send(JSON.stringify({
-            type: 'CONNECT',
-            player: {
-                name: playerName
-            },
-        }))
+  name = document.getElementById("name");
+  playerName = name.value;
+  // websocket = new WebSocket('wss://traviansserver.herokuapp.com/ws');
+  socket = io("http://localhost:3000");
+  socket.on("connect", onConnected);
+  socket.on("message", (data) => console.warn("socket.on message: " + data));
+  socket.on("disconnected", handleDisconnected);
+  socket.on("players:all", (data) => {
+    console.log(data);
+  });
 
-    };
-    websocket.onmessage = handleGetMessage
+  socket.on("players:connect", handleConnected);
+  socket.on("terrain:info", (data) => {
+    console.log(data);
+    terrain = data;
+  });
+}
 
-    websocket.onclose = handleDisconnected
+function testWS(string) {
+  socket.emit(string);
+}
+
+function onConnected() {
+  socket.emit("players:connect", {
+    player: {
+      name: playerName,
+    },
+  });
+  socket.emit("terrain:info");
 }
 
 function move(newPosition) {
-    websocket.send(JSON.stringify({
-        type: 'move',
-        player: {
-            name: playerName
-        },
-        move: newPosition
-    }))
+  socket.emit("players:move", {
+    player: {
+      name: playerName,
+    },
+    move: newPosition,
+  });
 }
 
 function handleConnected() {
-    const login = document.getElementById('login')
-    const game = document.getElementById('game')
+  const login = document.getElementById("login");
+  const game = document.getElementById("game");
 
-    game.classList.remove('hidden')
-    game.classList.add('visible')
-    login.classList.remove('visible')
-    login.classList.add('hidden')
+  game.classList.remove("hidden");
+  game.classList.add("visible");
+  login.classList.remove("visible");
+  login.classList.add("hidden");
 }
 
 function handleDisconnected() {
-    const login = document.getElementById('login')
-    const game = document.getElementById('game')
+  const login = document.getElementById("login");
+  const game = document.getElementById("game");
 
-    game.classList.add('hidden')
-    game.classList.remove('visible')
-    login.classList.add('visible')
-    login.classList.remove('hidden')
+  game.classList.add("hidden");
+  game.classList.remove("visible");
+  login.classList.add("visible");
+  login.classList.remove("hidden");
 }
 
-function handleGetMessage(event) {
-    if (event.data == "ERROR") {
-        connected = false
-        return
-    }
-    if (event.data == "CONNECTED") {
-        connected = true
-        handleConnected()
-        return
-    }
-    console.log(event.data)
-    const incomingData = JSON.parse(event.data)
-    if(incomingData.type == "TERRAIN") {
-        terrain = incomingData.data
-        return
-    }
-    if(incomingData.type == "PLAYERS") {
-        players = incomingData.data
-        const thisPlayer = players.find(pl => pl.name == playerName)
-        X = thisPlayer.x
-        Y = thisPlayer.y
-        return
-    }
-
-
-}
+// function handleGetMessage(event) {
+//   console.log(event);
+//   if (event.data == "ERROR") {
+//     connected = false;
+//     return;
+//   }
+//   if (event.data == "CONNECTED") {
+//     connected = true;
+//     handleConnected();
+//     return;
+//   }
+//   console.log(event.data);
+//   const incomingData = JSON.parse(event.data);
+//   if (incomingData.type == "TERRAIN") {
+//     terrain = incomingData.data;
+//     return;
+//   }
+//   if (incomingData.type == "PLAYERS") {
+//     players = incomingData.data;
+//     const thisPlayer = players.find((pl) => pl.name == playerName);
+//     X = thisPlayer.x;
+//     Y = thisPlayer.y;
+//     return;
+//   }
+// }
 
 function keyPressed() {
-    movePlayer()
+  movePlayer();
 }
 
-
-const mvmspeed = 1
+const mvmspeed = 1;
 
 function movePlayer() {
-    if(!terrain)return
-    localX = X
-    localY = Y
-    if (keyCode === LEFT_ARROW) {
-        localX -= mvmspeed
-    } else if (keyCode === RIGHT_ARROW) {
-        localX += mvmspeed
-    } else if (keyCode === UP_ARROW) {
-        localY -= mvmspeed
-    } else if (keyCode === DOWN_ARROW) {
-        localY += mvmspeed
-    }
-    localX = clampNumber(localX, 0, terrain.width - 1)
-    localY = clampNumber(localY, 0, terrain.height - 1)
-    sendMovePlayer(playerName, localX, localY)
+  if (!terrain) return;
+  localX = X;
+  localY = Y;
+  if (keyCode === LEFT_ARROW) {
+    localX -= mvmspeed;
+  } else if (keyCode === RIGHT_ARROW) {
+    localX += mvmspeed;
+  } else if (keyCode === UP_ARROW) {
+    localY -= mvmspeed;
+  } else if (keyCode === DOWN_ARROW) {
+    localY += mvmspeed;
+  }
+  localX = clampNumber(localX, 0, terrain.width - 1);
+  localY = clampNumber(localY, 0, terrain.height - 1);
+  sendMovePlayer(playerName, localX, localY);
 }
 
-function sendMovePlayer(name, x, y){
-    if(websocket){
-        websocket.send(JSON.stringify({
-            type:"MOVE",
-            player: {name},
-            move: {x,y}
-        }))
-    }
+function sendMovePlayer(name, x, y) {
+  if (socket) {
+    socket.emit("players:move", {
+      player: { name },
+      move: { x, y },
+    });
+  }
 }
 
 function clampNumber(num, min, max) {
-    return Math.min(Math.max(num, min), max);
+  return Math.min(Math.max(num, min), max);
 }
--- graphicsHandler.js
+++ graphicsHandler.js
@@ -1,204 +1,249 @@
-let blockSize = 3
-const frustumSize = 13
+let blockSize = 3;
+const frustumSize = 13;
 
 function drawChunk(chunk) {
-    const chunkSize = terrain.chunkSize
-    const chunkX = chunk.x * blockSize
-    const chunkY = chunk.y * blockSize
-    // strokeWeight(5)
-    // stroke("#f00")
-    // fill("#aaa")
-    rect(chunkX, chunkY, chunkSize * blockSize, chunkSize * blockSize)
+  const chunkSize = terrain.chunkSize;
+  const chunkX = chunk.x * blockSize;
+  const chunkY = chunk.y * blockSize;
+  // strokeWeight(5)
+  // stroke("#f00")
+  // fill("#aaa")
+  rect(chunkX, chunkY, chunkSize * blockSize, chunkSize * blockSize);
 }
 
 function drawBlock(block) {
-    // strokeWeight(0.5)
-    // stroke("#222")
-    fill(getBlockColor(block))
-    rect(block.x * blockSize, block.y * blockSize, blockSize, blockSize)
-    fill(255)
-    // text(`${block.x},${block.y}`,block.x * blockSize+2,block.y * blockSize+textSize())
-    moistureImg = mapImageToString(block.moisture, MOISTURE)
-    if (moistureImg) {
-        image(moistureImg, block.x * blockSize, block.y * blockSize, blockSize, blockSize)
-    }
-    materialRichnessImg = mapImageToString(block.materialRichness, MATERIAL_RICHNESS)
-    if (materialRichnessImg) {
-        image(materialRichnessImg, block.x * blockSize, block.y * blockSize, blockSize, blockSize)
-    }
-    animalsImg = mapImageToString(block.animals, ANIMALS)
-    if (animalsImg) {
-        image(animalsImg, block.x * blockSize, block.y * blockSize, blockSize, blockSize)
-    }
+  // strokeWeight(0.5)
+  // stroke("#222")
+  fill(getBlockColor(block));
+  rect(block.x * blockSize, block.y * blockSize, blockSize, blockSize);
+  fill(255);
+  // text(`${block.x},${block.y}`,block.x * blockSize+2,block.y * blockSize+textSize())
+  moistureImg = mapImageToString(block.moisture, MOISTURE);
+  if (moistureImg) {
+    image(
+      moistureImg,
+      block.x * blockSize,
+      block.y * blockSize,
+      blockSize,
+      blockSize
+    );
+  }
+  materialRichnessImg = mapImageToString(
+    block.materialRichness,
+    MATERIAL_RICHNESS
+  );
+  if (materialRichnessImg) {
+    image(
+      materialRichnessImg,
+      block.x * blockSize,
+      block.y * blockSize,
+      blockSize,
+      blockSize
+    );
+  }
+  animalsImg = mapImageToString(block.animals, ANIMALS);
+  if (animalsImg) {
+    image(
+      animalsImg,
+      block.x * blockSize,
+      block.y * blockSize,
+      blockSize,
+      blockSize
+    );
+  }
 }
-
 
 function mapImageToString(type, sourceEnum) {
-    string = sourceEnum[type]
-    switch (string) {
-        case 'COPPER':
-            return IMAGES.copper
+  string = sourceEnum[type];
+  switch (string) {
+    case "COPPER":
+      return IMAGES.copper;
 
-        case 'DEER':
-            return IMAGES.deer
+    case "DEER":
+      return IMAGES.deer;
 
-        case 'DESERT':
-            return IMAGES.desert
+    case "DESERT":
+      return IMAGES.desert;
 
-        case 'FIELD':
-            return IMAGES.field
+    case "FIELD":
+      return IMAGES.field;
 
-        case 'FISH':
-            return IMAGES.fish
+    case "FISH":
+      return IMAGES.fish;
 
-        case 'FOREST':
-            return IMAGES.forest
+    case "FOREST":
+      return IMAGES.forest;
 
-        case 'GOLD':
-            return IMAGES.gold
+    case "GOLD":
+      return IMAGES.gold;
 
-        case 'IRON':
-            return IMAGES.iron
+    case "IRON":
+      return IMAGES.iron;
 
-        default:
-            break;
-    }
+    default:
+      break;
+  }
 }
-let IMAGES = {}
+let IMAGES = {};
 
 function preload() {
-    const copper = loadImage('assets/copper.png')
-    const deer = loadImage('assets/deer.png')
-    const desert = loadImage('assets/desert.png')
-    const field = loadImage('assets/field.png')
-    const fish = loadImage('assets/fish.png')
-    const forest = loadImage('assets/forest.png')
-    const gold = loadImage('assets/gold.png')
-    const iron = loadImage('assets/iron.png')
-    const knight = loadImage('assets/knight.png')
-    const knightSleeping = loadImage('assets/knightSleeping.png')
-    IMAGES = {
-        copper,
-        deer,
-        desert,
-        field,
-        fish,
-        forest,
-        gold,
-        iron,
-        knight,
-        knightSleeping
-    }
-
+  const copper = loadImage("assets/copper.png");
+  const deer = loadImage("assets/deer.png");
+  const desert = loadImage("assets/desert.png");
+  const field = loadImage("assets/field.png");
+  const fish = loadImage("assets/fish.png");
+  const forest = loadImage("assets/forest.png");
+  const gold = loadImage("assets/gold.png");
+  const iron = loadImage("assets/iron.png");
+  const knight = loadImage("assets/knight.png");
+  const knightSleeping = loadImage("assets/knightSleeping.png");
+  IMAGES = {
+    copper,
+    deer,
+    desert,
+    field,
+    fish,
+    forest,
+    gold,
+    iron,
+    knight,
+    knightSleeping,
+  };
 }
 
 function setup() {
-    let c = createCanvas(500, 500)
+  let c = createCanvas(500, 500);
 
-    blockSize = width / frustumSize
-    c.parent('game')
-    textSize(10)
-    noStroke()
+  blockSize = width / frustumSize;
+  c.parent("game");
+  textSize(10);
+  noStroke();
 }
 
 function draw() {
-    background(0)
-    if (terrain) {
-        const frustum = getFrustum(X, Y)
-        const pointer = getPointer(frustum)
-        translate(-frustum.x * blockSize, -frustum.y * blockSize)
-        const chunks = terrain.chunks
-        for (chunkIndex = 0; chunkIndex < chunks.length; chunkIndex++) {
-            if (!chunks[chunkIndex]) continue
+  background(0);
+  if (terrain) {
+    const frustum = getFrustum(X, Y);
+    const pointer = getPointer(frustum);
+    translate(-frustum.x * blockSize, -frustum.y * blockSize);
+    const chunks = terrain.chunks;
+    for (chunkIndex = 0; chunkIndex < chunks.length; chunkIndex++) {
+      if (!chunks[chunkIndex]) continue;
 
-            const chunk = chunks[chunkIndex]
-            drawChunk(chunk)
-            for (blockIndex = 0; blockIndex < chunk.blocks.length; blockIndex++) {
-                const block = chunk.blocks[blockIndex]
-                drawBlock(block)
-                if(block.x == pointer.x/blockSize && block.y == pointer.y/blockSize) {
-                    selectedBlock = block
-                }
-            }
+      const chunk = chunks[chunkIndex];
+      drawChunk(chunk);
+      for (blockIndex = 0; blockIndex < chunk.blocks.length; blockIndex++) {
+        const block = chunk.blocks[blockIndex];
+        drawBlock(block);
+        if (
+          block.x == pointer.x / blockSize &&
+          block.y == pointer.y / blockSize
+        ) {
+          selectedBlock = block;
         }
-
-        drawPlayers()
-        drawFrustum(frustum)
-        drawPointer(pointer)
+      }
     }
+
+    drawPlayers();
+    drawFrustum(frustum);
+    drawPointer(pointer);
+  }
 }
 
 function drawFrustum(frustum) {
-    noFill()
-    // stroke("#00f")
-    rect(frustum.x * blockSize, frustum.y * blockSize, frustumSize * blockSize, frustumSize * blockSize)
-
+  noFill();
+  // stroke("#00f")
+  rect(
+    frustum.x * blockSize,
+    frustum.y * blockSize,
+    frustumSize * blockSize,
+    frustumSize * blockSize
+  );
 }
 
 function getFrustum(X, Y) {
-    let x = clampNumber(X - Math.floor(frustumSize / 2), 0, terrain.width - frustumSize)
-    let y = clampNumber(Y - Math.floor(frustumSize / 2), 0, terrain.height - frustumSize)
-    return {
-        x,
-        y
-    }
+  let x = clampNumber(
+    X - Math.floor(frustumSize / 2),
+    0,
+    terrain.width - frustumSize
+  );
+  let y = clampNumber(
+    Y - Math.floor(frustumSize / 2),
+    0,
+    terrain.height - frustumSize
+  );
+  return {
+    x,
+    y,
+  };
 }
 
 function getPointer(frustum) {
-    return {x:Math.floor(frustum.x+mouseX/blockSize)*blockSize, y:Math.floor(frustum.y+mouseY/blockSize)*blockSize}
+  return {
+    x: Math.floor(frustum.x + mouseX / blockSize) * blockSize,
+    y: Math.floor(frustum.y + mouseY / blockSize) * blockSize,
+  };
 }
 
 function drawPlayers() {
-    push()
-    fill("white")
-    stroke("black")
-    textSize(10)
-    for (let i = 0; i < players.length; i++) {
-        const player = players[i]
-        if (player.active) {
-            image(IMAGES.knight, player.x * blockSize, player.y * blockSize, blockSize, blockSize)
-        } else {
-            image(IMAGES.knightSleeping, player.x * blockSize, player.y * blockSize, blockSize, blockSize)
-        }
-        text(player.name, player.x * blockSize, player.y * blockSize)
+  push();
+  fill("white");
+  stroke("black");
+  textSize(10);
+  for (let i = 0; i < players.length; i++) {
+    const player = players[i];
+    if (player.active) {
+      image(
+        IMAGES.knight,
+        player.x * blockSize,
+        player.y * blockSize,
+        blockSize,
+        blockSize
+      );
+    } else {
+      image(
+        IMAGES.knightSleeping,
+        player.x * blockSize,
+        player.y * blockSize,
+        blockSize,
+        blockSize
+      );
     }
-    pop()
+    text(player.name, player.x * blockSize, player.y * blockSize);
+  }
+  pop();
 }
 
 function drawPointer(pointer) {
-    push()
-    stroke("red")
-    strokeWeight(2)
-    rect(pointer.x, pointer.y, blockSize, blockSize)
-    pop()
+  push();
+  stroke("red");
+  strokeWeight(2);
+  rect(pointer.x, pointer.y, blockSize, blockSize);
+  pop();
 }
-
 
 function getBlockColor(block) {
-    if (BIOMES[block.type] == "MOUNTAIN") return 'gray'
-    if (BIOMES[block.type] == "PLAIN") return 'darkgreen'
-    if (BIOMES[block.type] == "BEACH") return 'yellow'
-    if (BIOMES[block.type] == "WATER") return 'darkblue'
-    return block.type
+  if (BIOMES[block.type] == "MOUNTAIN") return "gray";
+  if (BIOMES[block.type] == "PLAIN") return "darkgreen";
+  if (BIOMES[block.type] == "BEACH") return "yellow";
+  if (BIOMES[block.type] == "WATER") return "darkblue";
+  return block.type;
 }
 
-
 function clampNumber(num, min, max) {
-    return Math.min(Math.max(num, min), max);
+  return Math.min(Math.max(num, min), max);
 }
 
 function movePlayer() {
-    if (keyCode === LEFT_ARROW) {
-        X -= 1
-    } else if (keyCode === RIGHT_ARROW) {
-        X += 1
-    } else if (keyCode === UP_ARROW) {
-        Y -= 1
-    } else if (keyCode === DOWN_ARROW) {
-        Y += 1
-    }
-    X = clampNumber(X, 0, terrain.width - 1)
-    Y = clampNumber(Y, 0, terrain.height - 1)
+  if (keyCode === LEFT_ARROW) {
+    X -= 1;
+  } else if (keyCode === RIGHT_ARROW) {
+    X += 1;
+  } else if (keyCode === UP_ARROW) {
+    Y -= 1;
+  } else if (keyCode === DOWN_ARROW) {
+    Y += 1;
+  }
+  X = clampNumber(X, 0, terrain.width - 1);
+  Y = clampNumber(Y, 0, terrain.height - 1);
 }
-
-
