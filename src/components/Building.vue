489f6b15f5325e55a3aa0b1db9610ae1bc785de4
jacekku <jacekwyroba97@gmail.com>
2021-09-18 14:24:14 +0200

    feat: Migration to vue - migrating components

---
+++ src/components/Building.vue
@@ -0,0 +1,5 @@
+<template v-if="currentPanel === 'building'">
+  <div class="building" style="visibility: visible">
+    <div class="building-possibilities"></div>
+  </div>
+</template>
---
+++ src/components/Crafting.vue
@@ -0,0 +1,5 @@
+<template v-if="currentPanel === 'crafting'">
+  <div class="crafting" style="visibility: visible">
+    <div class="crafting-possibilities"></div>
+  </div>
+</template>
